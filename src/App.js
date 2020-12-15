a7abf7044e38b8b002ba26f80d367ef0d66cc484
Eric Siroker <eric@scribe.ai>
2020-12-14 22:19:01 -0700

    Move logic into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
+import speakers from "./speakers";
+import transcripts from "./transcripts";
+
+export default function App() {
+  for (const transcript of transcripts) {
+    const speakerNames = transcript.speakerIds.map(findSpeakerName).join(', ');
+    console.log(`${speakerNames}:`, transcript.text);
+  }
   return <div>Scribe Search</div>;
 }
 
-export default App;
+function findSpeakerName(speakerId) {
+  const speaker = speakers.find((speaker) => speaker.id === speakerId);
+  return speaker?.name ?? 'Unknown Speaker';
+}
--- src/index.js
+++ src/index.js
@@ -2,17 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import speakers from './speakers';
-import transcripts from './transcripts';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-function findSpeakerName(speakerId) {
-  const speaker = speakers.find((speaker) => speaker.id === speakerId);
-  return speaker?.name ?? 'Unknown Speaker';
-}
-
-for (const transcript of transcripts) {
-  const speakerNames = transcript.speakerIds.map(findSpeakerName).join(', ');
-  console.log(`${speakerNames}:`, transcript.text);
-}
